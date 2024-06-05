97f2db65a0483b7ded81975cd71f0b791e1d0baa
Eduardo Canche Vazquez <ecanchev@tibco.com>
2024-06-05 12:40:00 -0600

    removed esbuild-saas from test-app

--- packages/test-app/styles/styles.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright © 2005-2024. Cloud Software Group, Inc. All rights reserved. Confidential & Proprietary.
- * Licensed pursuant to commercial Cloud Software Group, Inc End User License Agreement.
- */
-
-// the following file is necessary to generate the styles with esbuild:
-import "../../jrs-ui-components/scss/jasper-ui.scss";
