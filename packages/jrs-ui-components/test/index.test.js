e2174df94d034d2237c90f37e76c23181d581a25
Eduardo Canche Vazquez <ecanchev@tibco.com>
2024-06-13 17:29:55 -0600

    component renamed on the tests

--- packages/jrs-ui-components/test/index.test.js
+++ packages/jrs-ui-components/test/index.test.js
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import { JVButton as JRSButton } from 'jrs-ui-components/material-ui/Button/Button';
+import { JVButton } from 'jrs-ui-components/material-ui/Button/Button';
 import '@testing-library/jest-dom';
 
 describe("JVButton tests", () => {
     it("should render JVButton", () => {
-        render(<JRSButton />);
+        render(<JVButton />);
         const buttonElement = screen.getByRole('button');
         expect(buttonElement).toBeInTheDocument();
     });
