9062aec5b510d337fc28108b23762ed6a3845020
Grant Bacon <gbacon@tibco.com>
2024-06-24 13:02:26 -0700

    use ts-jest instead of babel-jest for input-control suite

--- packages/jv-input-controls/jest.config.ts
+++ packages/jv-input-controls/jest.config.ts
@@ -1,31 +1,17 @@
 import { Config } from 'jest';
 
 const conf: Config = {
-    verbose: true,
-    watchPathIgnorePatterns: [
-        'node_modules',
-        'dist',
-        'coverage',
-        'build',
-    ],
-    testEnvironment: 'jsdom',
-    transform: {
-        '^.+\\.([tj]s|[tj]sx|mjs)$': 'babel-jest',
-    },
-    transformIgnorePatterns: [
-        'node_modules/(?!(\\@babel/runtime/helpers/esm|\\@mui/material|\\@mui/icons-material|\\@jaspersoft)/)',
-    ],
-    testMatch: [
-        "**/__tests__/**/*.[jt]s?(x)",
-        "**/?(*.)+(spec|test).[tj]s?(x)",
-        "**/?(*.)+(spec|test).mjs"
-    ],
-    moduleNameMapper: {
-        '^@jaspersoft/jv-ui-components(.*)$': '<rootDir>/node_modules/@jaspersoft/jv-ui-components/dist/material-ui$1',
-        '^@mui/material/(.*)$': '<rootDir>/../jv-ui-components/node_modules/@mui/material/$1',
-        '^@mui/icons-material/(.*)$': '<rootDir>/../jv-ui-components/node_modules/@mui/icons-material/$1',
-    },
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'mjs'],
+  verbose: true,
+  watchPathIgnorePatterns: [
+    'node_modules',
+    'dist',
+    'coverage',
+    'build',
+  ],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  }
 };
 
 export default conf;
