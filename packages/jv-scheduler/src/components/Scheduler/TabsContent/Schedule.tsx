098da702988f09ba88c7e2e511030e9522ce79a1
yapte <yapte@tibco.com>
2024-07-05 16:03:35 +0530

    Enabled start date only when Specific date is selected and added Select options for timeframe

--- packages/jv-scheduler/src/components/Scheduler/TabsContent/Schedule.tsx
+++ packages/jv-scheduler/src/components/Scheduler/TabsContent/Schedule.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import {JVTextField, JVTypography, JVRadioGroup, JVRadioButton} from "@jaspersoft/jv-ui-components";
+import React, {useState} from 'react';
+import {JVTextField, JVTypography, JVRadioGroup, JVRadioButton, JVSelectItem} from "@jaspersoft/jv-ui-components";
+import {timeFrames} from "../../../constants/schedulerConstants";
 
 const Schedule = () => {
+    const [selectedValue, setSelectedValue] = useState("option1");
+    const handleRadioChange = (value) => {
+        setSelectedValue(value);
+    }
     return (
         <>
             <JVTypography variant="h6">Recurrence</JVTypography>
@@ -24,8 +29,12 @@ const Schedule = () => {
                         label="Timeframe (required)"
                         textFieldClassName="jv-uWidth-175px"
                         select
-                        defaultValue="Minutes"
-                    />
+
+                    >
+                        {
+                            timeFrames.map((timeFrame) => <JVSelectItem key={timeFrame.value} value={timeFrame.value} label="Days" >{timeFrame.textPlural}</JVSelectItem>)
+                        }
+                    </JVTextField>
                 </div>
             </div>
             <JVRadioGroup
@@ -33,21 +42,27 @@ const Schedule = () => {
 
             >
                 <JVRadioButton
+                    id="now"
                     value="now"
                     label="Now"
-                    checked
+                    checked={selectedValue === "option1"}
+                    onChange={() => handleRadioChange("option1")}
                 />
                 <JVRadioButton
-                    value="now"
+                    id="specificDate"
+                    value="specificDate"
                     label="Specific date and time"
+                    checked={selectedValue === "option2"}
+                    onChange={() => handleRadioChange("option2")}
                 />
             </JVRadioGroup>
-            <div className="jr-uMargin-l-07 jr-uWidth-200px">
+            <div className="jv-uMargin-l-07 jv-uWidth-200px">
                 <JVTextField
                     size="large"
                     label="Start date"
                     type="date"
                     defaultValue="2024-07-02"
+                    disabled={selectedValue !== "option2"}
                 />
             </div>
 
