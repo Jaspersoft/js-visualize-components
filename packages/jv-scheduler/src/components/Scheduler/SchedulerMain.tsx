fe8568308b9b6e22d853412f87f39e970f8f3182
anthomba <anthomba@tibco.com>
2024-07-05 12:23:41 +0530

    adding services for output, timezone , locale and input controls

--- packages/jv-scheduler/src/components/Scheduler/SchedulerMain.tsx
+++ packages/jv-scheduler/src/components/Scheduler/SchedulerMain.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { useTranslation } from 'react-i18next';
 import "@jaspersoft/jv-ui-components/dist/jv-ui.css";
 import {JVTabs, JVTab} from "@jaspersoft/jv-ui-components";
@@ -8,11 +8,40 @@ import Parameters from "./TabsContent/Parameters";
 import Output from "./TabsContent/Output";
 import Notifications from "./TabsContent/Notifications";
 import SchedulerFooter from "../SchedulerFooter";
+import {
+    getUserTimezones,
+    getOutputOptions,
+    getSupportedLocale,
+    getInputControls
+} from "../../services/schedulerServices"
 
 
 const SchedulerMain = () => {
     const [value, setValue] = useState<number>(0);
     const { t } = useTranslation();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const timezones = await getUserTimezones();
+            const outputOptions = await getOutputOptions();
+            const locale = await getSupportedLocale();
+            const inputControls = await getInputControls();
+            if (timezones.error) {
+                // handle error for user timezones
+            }
+
+            if (outputOptions.error) {
+                // handle error for output options
+            }
+            if(locale.error){
+                // handle error for locale
+            }
+            if(inputControls.error){
+                // handle error for input controls
+            }
+        }
+        fetchData();
+    }, [])
     const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
         setValue(newValue);
     };
