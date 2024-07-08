82e559f31718a3afc1fa5fe079ee30880f0c8a81
yapte <yapte@tibco.com>
2024-07-08 16:11:04 +0530

    Added Radio group to notifications tab and enable/disable based on which radio button is selected.

--- packages/jv-scheduler/src/components/Tabs/TabsContent/Notifications.tsx
+++ packages/jv-scheduler/src/components/Tabs/TabsContent/Notifications.tsx
@@ -1,7 +1,17 @@
-import React from "react";
-import { JVTextField, JVTypography } from "@jaspersoft/jv-ui-components";
+import React, { useState } from "react";
+import {
+  JVButton,
+  JVRadioButton,
+  JVRadioGroup,
+  JVTextField,
+  JVTypography,
+} from "@jaspersoft/jv-ui-components";
 
 const Notifications = () => {
+  const [selectedValue, setSelectedValue] = useState("option1");
+  const handleRadioChange = (value) => {
+    setSelectedValue(value);
+  };
   return (
     <>
       <JVTypography variant="h6">Email Notification</JVTypography>
@@ -13,6 +23,36 @@ const Notifications = () => {
         />
         <JVTextField size="large" label="Subject (required)" />
         <JVTextField size="large" label="Message" multiline rows={5} />
+        <JVRadioGroup title="Report access (required)">
+          <JVRadioButton
+            value="option1"
+            label="Include report as repository link."
+            checked={selectedValue === "option1"}
+            onChange={() => handleRadioChange("option1")}
+          />
+
+          <div className="jv-mInput jv-mInputBrowse jv-mInputLarge jv-uMargin-l-07 mui">
+            <JVTextField
+              label="Repository URI"
+              disabled={selectedValue !== "option1"}
+              value="/path/"
+            />
+            <JVButton
+              variant="contained"
+              size="large"
+              disabled={selectedValue !== "option1"}
+            >
+              Browse...
+            </JVButton>
+          </div>
+
+          <JVRadioButton
+            value="option2"
+            label="Include report file as attachment."
+            checked={selectedValue === "option2"}
+            onChange={() => handleRadioChange("option2")}
+          />
+        </JVRadioGroup>
       </div>
     </>
   );
