7fe3df5556c93f37bd9db02c367457fcff534cf1
Kumar Manas <kmanas@tibco.com>
2024-07-09 08:49:30 +0530

    passed props

--- packages/jv-scheduler/src/components/EntryPoint.tsx
+++ packages/jv-scheduler/src/components/EntryPoint.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 import store from "./../store/store";
-import { Provider as ReduxProvider } from 'react-redux';
-import SchedulerMain from "./SchedulerMain"
+import { Provider as ReduxProvider } from "react-redux";
+import SchedulerMain from "./SchedulerMain";
 
-type EntryPointProps = { lang: string }
+type EntryPointProps = { lang: string };
 const EntryPoint = (props: EntryPointProps) => {
-    console.log(props)
-    return (
-        <ReduxProvider store={store}>
-            <SchedulerMain />
-        </ReduxProvider>
-    )
-}
+  console.log(props);
+  return (
+    <ReduxProvider store={store}>
+      <SchedulerMain lang={props.lang} />
+    </ReduxProvider>
+  );
+};
 
 export default EntryPoint;
