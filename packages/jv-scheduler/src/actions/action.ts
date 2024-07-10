dbe5014a55976cc621105019c70b1c37d4cdeb04
anthomba <anthomba@tibco.com>
2024-07-10 14:50:12 +0530

    converting api call to thunk actions

--- packages/jv-scheduler/src/actions/action.ts
+++ packages/jv-scheduler/src/actions/action.ts
@@ -2,6 +2,10 @@ import {
   SET_OUTPUT_FORMATS,
   SET_USER_TIME_ZONES,
 } from "../constants/actionConstants";
+import {
+  getOutputFormatsFromService,
+  getUserTimezonesFromService,
+} from "../services/schedulerServices";
 
 // export const setUserLocale = (supportedLocale) => {
 //    return {
@@ -27,5 +31,27 @@ export const setUserTimeZones = (timeZones) => {
     payload: {
       userTimeZones: timeZones,
     },
+  };
+};
+
+export const getOutputFormats = () => {
+  return async (dispatch) => {
+    const outputFormats = await getOutputFormatsFromService();
+    if (outputFormats.error) {
+      // dispatch error action
+    } else {
+      dispatch(setOutputFormats(outputFormats.dashboard.outputFormats));
+    }
+  };
+};
+
+export const getUserTimeZones = () => {
+  return async (dispatch) => {
+    const timezones = await getUserTimezonesFromService();
+    if (timezones.error) {
+      // dispatch error action
+    } else {
+      dispatch(setUserTimeZones(timezones));
+    }
   };
 };
--- packages/jv-scheduler/src/components/Tabs/Tabs.tsx
+++ packages/jv-scheduler/src/components/Tabs/Tabs.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import "@jaspersoft/jv-ui-components/dist/jv-ui.css";
 import { JVTabs, JVTab } from "@jaspersoft/jv-ui-components";
@@ -8,45 +8,13 @@ import Parameters from "../Tabs/TabsContent/Parameters";
 import Output from "../Tabs/TabsContent/Output";
 import Notifications from "../Tabs/TabsContent/Notifications";
 import SchedulerFooter from "./SchedulerFooter";
-import {
-  getUserTimezones,
-  getOutputOptions,
-  getInputControls,
-} from "../../services/schedulerServices";
-// import {useDispatch} from "react-redux";
-import { setUserTimeZones } from "../../actions/action";
 import ScheduleStepper from "../Stepper/ScheduleStepper";
 import SchedulerHeader from "./SchedulerHeader";
 
 const Tabs = () => {
   const [value, setValue] = useState<number>(0);
-  // const dispatch = useDispatch();
   const { t } = useTranslation();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const timezones = await getUserTimezones();
-      const outputOptions = await getOutputOptions();
-      // const locale = await getSupportedLocale();
-      const inputControls = await getInputControls();
-      if (timezones.error) {
-        // handle error for user timezones
-      } else {
-        // dispatch(setUserTimeZones(timezones))
-      }
-
-      if (outputOptions.error) {
-        // handle error for output options
-      }
-      // if(locale.error){
-      //     // handle error for locale
-      // }
-      if (inputControls.error) {
-        // handle error for input controls
-      }
-    };
-    fetchData();
-  }, []);
   const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
     setValue(newValue);
   };
