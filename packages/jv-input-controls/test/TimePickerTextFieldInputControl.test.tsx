bdd73e46ef8b279d204445d18f2f5053c0ec3795
Eduardo Canche Vazquez <ecanchev@tibco.com>
2024-07-17 11:23:03 -0600

    TimePickerTextFieldInputControl tests, added

---
+++ packages/jv-input-controls/test/TimePickerTextFieldInputControl.test.tsx
@@ -0,0 +1,128 @@
+import { SizeToClass } from "@jaspersoft/jv-ui-components/material-ui/types/InputTypes";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { JSX } from "react";
+import "@testing-library/jest-dom";
+import { TimePickerTextFieldInputControl } from "../src/controls/TimePickerTextFieldInputControl";
+
+const LARGE_CSS_CLASS = SizeToClass.large;
+const requiredProps = {
+  id: "column_time_1",
+  label: "column_time",
+  mandatory: false,
+  readOnly: false,
+  visible: true,
+  type: "singleValueTime",
+};
+
+const getTimePicketTextFieldIC = (options?: object): JSX.Element => {
+  return (
+    <TimePickerTextFieldInputControl {...{ ...requiredProps, ...options }} />
+  );
+};
+
+describe("TimePickerTextFieldInputControl tests", () => {
+  test("TimePickerTextFieldInputControl is rendered correctly", () => {
+    const { container } = render(getTimePicketTextFieldIC());
+    const buttonElement = container.querySelector("input");
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  // Test for label prop
+  test("displays the label when provided", () => {
+    const testLabel = "Test Label";
+    render(getTimePicketTextFieldIC({ label: testLabel }));
+    const labelElement = screen.queryByLabelText(testLabel);
+    expect(labelElement).toBeInTheDocument();
+  });
+
+  // Test for value prop
+  test("uses value as the initial input value", () => {
+    const defaultValue = "23:44:21";
+    const { container } = render(getTimePicketTextFieldIC({ defaultValue }));
+
+    const inputElement = container.querySelector("input") as HTMLInputElement;
+    expect(inputElement.value).toBe(defaultValue);
+  });
+
+  // Test for onChange event
+  test("updates value on change", () => {
+    const { container } = render(getTimePicketTextFieldIC({}));
+    const inputElement = container.querySelector("input") as HTMLInputElement;
+    const newValue = "11:22:16";
+    fireEvent.change(inputElement, { target: { value: newValue } });
+    expect(inputElement.value).toBe(newValue);
+  });
+
+  // Test for variant prop
+  test("changes style based on variant prop", () => {
+    const { container } = render(
+      getTimePicketTextFieldIC({ variant: "outlined" }),
+    );
+    let inputElement = container.querySelector("input") as HTMLInputElement;
+    expect(inputElement).toHaveClass("MuiOutlinedInput-input");
+  });
+
+  // test for default size.
+  test("check the default size is large if it is not provided", () => {
+    // Render the component
+    const { container } = render(getTimePicketTextFieldIC({}));
+
+    // Use querySelector to get the first div with the class "jv-mInputLarge"
+    const divElement = container.querySelector(`div.${LARGE_CSS_CLASS}`);
+
+    // Assert that the element is found and has the expected class
+    expect(divElement).toBeInTheDocument();
+    expect(divElement).toHaveClass(LARGE_CSS_CLASS);
+  });
+
+  // test className prop
+  test("check wrapping CSS class", () => {
+    const cssClass = "jv-mInput";
+    // Render the component
+    const { container } = render(
+      getTimePicketTextFieldIC({ className: cssClass }),
+    );
+
+    // Use querySelector to get the first div with the class "jv-mInputLarge"
+    const inputElement = container.querySelector(
+      `.${cssClass}`,
+    ) as HTMLInputElement;
+    // Assert that the element is found and has the expected class
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement).toHaveClass(cssClass);
+  });
+
+  // test readOnly prop
+  test("check the component is read-only", () => {
+    // Render the component
+    const { container } = render(getTimePicketTextFieldIC({ readOnly: true }));
+    let inputElement = container.querySelector("input") as HTMLInputElement;
+
+    // Assert that the element is found and has the expected attribute
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement).toHaveAttribute("readonly");
+  });
+
+  // test visible prop
+  test("check the component is visible or not", () => {
+    const HIDDEN_CLASS_NAME = "jv-uVisibility-hide";
+    // Render the component
+    const { container } = render(getTimePicketTextFieldIC({ visible: false }));
+    // Use querySelector to get the first div with the class "jv-mInputLarge"
+    const divElement = container.querySelector(`div.${HIDDEN_CLASS_NAME}`);
+
+    // Assert that the element is found and has the expected class
+    expect(divElement).toBeInTheDocument();
+    expect(divElement).toHaveClass(HIDDEN_CLASS_NAME);
+  });
+
+  // Test for mandatory field
+  test("verify the field shows error when mandatory prop is set", () => {
+    const CSS_ERROR_CLASS = "jv-uMandatory";
+    const { container } = render(getTimePicketTextFieldIC({ mandatory: true }));
+    let wrapperDiv = container.querySelector(
+      `div.${CSS_ERROR_CLASS}`,
+    ) as HTMLInputElement;
+    expect(wrapperDiv).toBeInTheDocument();
+  });
+});
