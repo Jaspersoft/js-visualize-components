a0a686a3d31791084d1b2e5753c4c8fa0b99276f
yapte <yapte@tibco.com>
2024-07-17 14:34:55 +0530

    Text for individual steps

--- packages/jv-scheduler/src/components/Stepper/DefaultData.tsx
+++ packages/jv-scheduler/src/components/Stepper/DefaultData.tsx
@@ -5,9 +5,40 @@ import { KeyValueTemplate } from "./KeyValueTemplate";
 export const ScheduleStepDefaultMessage = () => {
   return (
     <>
-      <FieldHeader text={"Name the scheduled Job"} />
+      <FieldHeader text="Name the scheduled Job" />
       <KeyValueTemplate title="Repeat every:" value="1 day" />
       <KeyValueTemplate title="Starting:" value="Now" />
+    </>
+  );
+};
+
+export const OutputStepDefaultMessage = () => {
+  return (
+    <>
+      <FieldHeader text="Customize the output or it will be set to the following:" />
+      <KeyValueTemplate title="File name:" value="fileName" />
+      <KeyValueTemplate title="Time zone:" value="timeZone" />
+      <KeyValueTemplate title="Format:" value="format" />
+    </>
+  );
+};
+
+export const NotificationStepDefaultMessage = () => {
+  return (
+    <>
+      <FieldHeader text="Specify recipients." />
+      <FieldHeader text="Enter a subject." />
+      <FieldHeader text="Add an optional message." />
+      <KeyValueTemplate title="Report access:" value="Link" />
+    </>
+  );
+};
+
+export const ParametersStepDefaultMessage = () => {
+  return (
+    <>
+      <FieldHeader text="Select the input control options for the report/dashboard." />
+      <KeyValueTemplate title="Brand:" value="brands" />
     </>
   );
 };
