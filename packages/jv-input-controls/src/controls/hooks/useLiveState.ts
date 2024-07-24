c162b41bdcd50c756139af52f5f19a6906d0b452
Grant Bacon <gbacon@tibco.com>
2024-07-24 12:51:51 -0700

    Add some comments for useLiveState usage

--- packages/jv-input-controls/src/controls/hooks/useLiveState.ts
+++ packages/jv-input-controls/src/controls/hooks/useLiveState.ts
@@ -1,6 +1,13 @@
 import { useState } from "react";
 
 export function useLiveState(initialValue: any) {
+  /**
+   * Changes the value of an input element to match the one selected
+   *
+   * @param initialValue The value to set initially
+   *
+   * @returns An object with two elements value and onChange which holds the value pointer and an onChange handler function to provide to a react element
+   */
   const [value, setValue] = useState(initialValue);
 
   function handleChange(e: any) {
