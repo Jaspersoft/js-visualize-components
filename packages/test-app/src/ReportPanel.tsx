6f5beacdb77692b4c16e428d60a32d4dad1c35fe
Grant Bacon <gbacon@tibco.com>
2024-08-13 09:29:21 -0700

    Add report viewer for single select ic to test-app

---
+++ packages/test-app/src/ReportPanel.tsx
@@ -0,0 +1,33 @@
+import {
+  BaseInputControlProps,
+  ICType,
+} from "@jaspersoft/jv-input-controls/src/controls/BaseInputControl";
+import { VisualizeType } from "@jaspersoft/jv-tools";
+import { useEffect, useState } from "react";
+
+interface ReportPanelProps {
+  reportUri?: string;
+  vObject?: VisualizeType;
+  controlState?: BaseInputControlProps[];
+}
+
+export default function ReportPanel(props: ReportPanelProps) {
+  const [vizReport, setVizReport] = useState();
+
+  useEffect(() => {
+    var report = props.vObject?.report({
+      resource: props.reportUri || "",
+      container: "#report-viewer",
+      events: {
+        reportCompleted: function (status) {},
+      },
+    });
+    setVizReport(report as any);
+  }, []);
+
+  if (vizReport && props.controlState) {
+    (vizReport as any).params(props.controlState).run();
+  }
+
+  return <div id="report-viewer"></div>;
+}
