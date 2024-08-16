7cfcba5d0139ccfd1d18007cafc31ac0efa54d6c
Eduardo Canche Vazquez <ecanchev@tibco.com>
2024-08-16 12:10:48 -0600

    removed unneeded method

--- packages/jv-input-controls/src/utils/ErrorMessageUtils.ts
+++ packages/jv-input-controls/src/utils/ErrorMessageUtils.ts
@@ -17,21 +17,3 @@ export const getMandatoryErrorMessage = (
   }
   return rule!.mandatoryValidationRule!.errorMessage || "";
 };
-
-export const getValidationRuleByKey = ({
-  validationRules,
-  key,
-}: {
-  validationRules: ICValidationRule[];
-  key:
-    | "dateTimeFormatValidationRule"
-    | "mandatoryValidationRule"
-    | "regexpValidationRule"
-    | "rangeValidationRule";
-}): null | ICValidationRule => {
-  if (!validationRules) {
-    return null;
-  }
-  const rule = validationRules.find((rule) => rule[key] !== undefined);
-  return rule || null;
-};
