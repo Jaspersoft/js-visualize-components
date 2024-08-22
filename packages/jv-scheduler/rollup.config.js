23bf237e5dffc0a01365343f052fd0c8ca7ca2ed
Kumar Manas <kmanas@tibco.com>
2024-08-22 11:57:08 +0530

    refactored and added typings

--- packages/jv-scheduler/rollup.config.js
+++ packages/jv-scheduler/rollup.config.js
@@ -1,37 +1,40 @@
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import postcss from 'rollup-plugin-postcss';
-import json from '@rollup/plugin-json';
+import postcss from "rollup-plugin-postcss";
+import json from "@rollup/plugin-json";
 
 const packageJson = require("./package.json");
 
 export default [
-    {
-        input: "src/index.ts",
-        external: Object.keys(packageJson.dependencies || {}),
-        output: [
-            {
-                file: packageJson.module,
-                format: "esm",
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            resolve(),
-            postcss({
-                extensions: ['.css'],
-            }),
-            typescript({
-                tsconfig: "./tsconfig.json" ,
-                include: ['**/*.ts+(|x)', '**/*.mts']
-            }),
-            json()
-        ],
-    },
-    {
-        input: "dist/esm/types/index.d.ts",
-        output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts()],
-    },
+  {
+    input: "src/index.ts",
+    external: [
+      "i18next-http-backend",
+      ...Object.keys(packageJson.dependencies || {}),
+    ],
+    output: [
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      postcss({
+        extensions: [".css"],
+      }),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        include: ["**/*.ts+(|x)", "**/*.mts"],
+      }),
+      json(),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
 ];
