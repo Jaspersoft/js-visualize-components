ea5e16cb1304a0fd68a9fac120660f248fbc2b30
Eduardo Canche Vazquez <ecanchev@tibco.com>
2024-08-28 13:29:54 -0600

    docs updated

--- packages/jv-input-controls/src/utils/ObjectUtils.ts
+++ packages/jv-input-controls/src/utils/ObjectUtils.ts
@@ -1,3 +1,11 @@
+/**
+ * Checks if the object doesn't have any keys. If the object is null or undefined, it returns true.
+ * @param obj
+ * @returns {boolean}
+ */
 export const isEmptyObject = (obj: any): boolean => {
+  if (obj === null || obj === undefined) {
+    return true;
+  }
   return Object.keys(obj).length === 0;
 };
