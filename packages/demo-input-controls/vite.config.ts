b5240f8815ae5f4438d17bc565930311715636bf
Eduardo Canche Vazquez <ecanchev@tibco.com>
2024-09-06 12:29:54 -0600

    vite.config file has been cleaned up

--- packages/demo-input-controls/vite.config.ts
+++ packages/demo-input-controls/vite.config.ts
@@ -1,23 +1,14 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import libCss from "vite-plugin-libcss";
-import * as packageJson from "./package.json";
+import { dependencies } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), libCss()],
   build: {
     rollupOptions: {
-      external: [
-        ...Object.keys(packageJson.dependencies || {}),
-        "react/jsx-runtime",
-      ],
-    },
-  },
-  resolve: {
-    alias: {
-      "@jaspersoft/jv-input-controls":
-        "../../../node_modules/@jaspersoft/jv-input-controls",
+      external: [...Object.keys(dependencies || {}), "react/jsx-runtime"],
     },
   },
 });
