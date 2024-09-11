19c7e2bf4bc554e0dd4bb5b5bc3113cfec31ee27
Eduardo Canche Vazquez <ecanchev@tibco.com>
2024-09-11 12:18:54 -0600

    files cleaned up after merging main into this branch

--- packages/test-app/src/TestReducer.ts
+++
@@ -1,9 +0,0 @@
-export default function TestReducer(state: any, action: any) {
-  console.log("state: ", state, " action: ", action);
-  if (action.type === "bump") {
-    if (state.store >= 10) state.store = 0;
-    return {
-      store: state.store + 1,
-    };
-  }
-}
