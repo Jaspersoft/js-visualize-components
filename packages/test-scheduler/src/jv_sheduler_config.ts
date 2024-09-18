ff0b67558e364e2cdbb1385fa1018b016a83cb5e
anthomba <anthomba@tibco.com>
2024-09-18 16:39:15 +0530

    refactoring code

--- packages/test-scheduler/src/jv_sheduler_config.ts
+++ packages/test-scheduler/src/jv_sheduler_config.ts
@@ -11,7 +11,7 @@ const schedulerUIConfig = {
       schedule: {
         defaultValues: {
           // name of the schedule: required in config
-          label: { showField: false },
+          label: { value: "aa cdn" },
           description: {
             value: "description test 1",
             showField: true,
