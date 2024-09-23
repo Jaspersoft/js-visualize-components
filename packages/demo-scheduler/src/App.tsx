1f9753ab24ccf53967b57ce0d152d8c0b241e055
anthomba <anthomba@tibco.com>
2024-09-23 15:22:15 +0530

    demo application

--- packages/demo-scheduler/src/App.tsx
+++ packages/demo-scheduler/src/App.tsx
@@ -8,11 +8,14 @@ import {
   VisualizeType,
 } from "@jaspersoft/jv-tools";
 import {
-  Scheduler,
-  SchedulerUiJS,
+  // SchedulerUiJS,
   SchedulerConfig,
 } from "@jaspersoft/jv-scheduler";
+import { JVButton } from "@jaspersoft/jv-ui-components";
 import schedulerUIConfig from "./jv_sheduler_config";
+import ReportPanel from "./report/ReportPanel";
+import "./App.css";
+import { Scheduler } from "./Scheduler";
 
 const visualizeUrl = `${schedulerUIConfig.server}/client/visualize.js`;
 
@@ -25,6 +28,8 @@ function SchedulerApp() {
   const [visualize, setVisualize] = useState(
     null as { v: VisualizeType } | null,
   );
+
+  const [isPanelOpen, setIsPanelOpen] = useState(false);
 
   useEffect(() => {
     const loadVisualize = visualizejsLoader(visualizeUrl);
@@ -41,20 +46,28 @@ function SchedulerApp() {
         },
       );
     });
+    schedulerUIConfig.events.cancelBtnClick = () => {
+      setIsPanelOpen(false);
+    };
+    schedulerUIConfig.events.success = () => {
+      console.log("Job created successfully");
+      setIsPanelOpen(false);
+    };
   }, []);
 
   useEffect(() => {
     if (visualize === undefined) {
       return;
     }
-    const schedulerElement = document.getElementById("scheduler");
-    if (schedulerElement && visualize) {
-      SchedulerUiJS(
-        schedulerElement as HTMLElement,
-        visualize,
-        schedulerUIConfig,
-      );
-    }
+    // uncomment below code to see working of scheduler using JS API
+    // const schedulerElement = document.getElementById("scheduler");
+    // if (schedulerElement && visualize) {
+    //   SchedulerUiJS(
+    //     schedulerElement as HTMLElement,
+    //     visualize,
+    //     schedulerUIConfig,
+    //   );
+    // }
   }, [visualize]);
 
   console.log(SchedulerConfig, "SchedulerConfig");
@@ -62,10 +75,47 @@ function SchedulerApp() {
     <>
       <div id="scheduler"></div>
       {visualize ? (
-        <Scheduler
-          schedulerUIConfig={schedulerUIConfig}
-          visualize={visualize}
-        />
+        <>
+          <div id="header">
+            <h1 className="flexItem pageHeader-title-text">
+              Scheduler App
+              <div className="pageHeader-subtitle-path">
+                <span className="pageHeader-subtitle-path-label">
+                  Schedule For:{" "}
+                </span>
+                <span className="pageHeader-subtitle-path-text">
+                  {schedulerUIConfig.resourceURI}
+                </span>
+              </div>
+            </h1>
+            <JVButton
+              id="scheduleBtn"
+              disableElevation
+              size="large"
+              color="primary"
+              variant="contained"
+              onClick={() => setIsPanelOpen(true)}
+            >
+              Scheduler
+            </JVButton>
+          </div>
+          <div
+            id="pageDimmer"
+            className={`dimmer ${isPanelOpen ? "" : "hidden"}`}
+            style={{ zIndex: 999 }}
+          ></div>
+          <ReportPanel
+            vObject={visualize.v}
+            resourceURI={schedulerUIConfig.resourceURI}
+          />
+          {isPanelOpen && (
+            <Scheduler
+              visualize={visualize}
+              schedulerUIConfig={schedulerUIConfig}
+              isPanelOpen={isPanelOpen}
+            />
+          )}
+        </>
       ) : (
         <div>Loading...</div>
       )}
--- packages/demo-scheduler/src/report/ReportPanel.tsx
+++ packages/demo-scheduler/src/report/ReportPanel.tsx
@@ -1,30 +1,21 @@
-import { BaseInputControlProps } from "@jaspersoft/jv-input-controls";
 import { VisualizeType } from "@jaspersoft/jv-tools";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 interface ReportPanelProps {
-  reportUri?: string;
+  resourceURI?: string;
   vObject?: VisualizeType;
-  controlState?: BaseInputControlProps[];
 }
 
 export default function ReportPanel(props: ReportPanelProps) {
-  const [vizReport, setVizReport] = useState();
-
   useEffect(() => {
-    var report = props.vObject?.report({
-      resource: props.reportUri || "",
+    props.vObject?.report({
+      resource: props.resourceURI || "",
       container: "#report-viewer",
       events: {
         reportCompleted: function (_status: any) {},
       },
     });
-    setVizReport(report as any);
   }, []);
-
-  if (vizReport && props.controlState) {
-    (vizReport as any).params(props.controlState).run();
-  }
 
   return <div id="report-viewer"></div>;
 }
