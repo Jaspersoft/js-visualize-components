45a5a016c11a53a901f94aa4a5e7f6c4b57daba2
Eduardo Canche Vazquez <ecanchev@tibco.com>
2024-10-03 15:41:37 -0600

    fix(jv-ui-components): add MulSelectInputControl tests
    add tests for the multi-select input control

---
+++ packages/jv-input-controls/test/controls/MultiSelectInputControl.test.tsx
@@ -0,0 +1,109 @@
+/*
+ * Copyright © 2024. Cloud Software Group, Inc.
+ * This file is subject to the license terms contained
+ * in the license file that is distributed with this file.
+ */
+import { screen } from "@testing-library/dom";
+import { act, cleanup, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MultiSelectInputControl } from "../../src/controls/MultiSelectInputControl";
+import "@testing-library/jest-dom";
+
+const requiredProps = {
+  id: "ProductFamily",
+  description: "Product Family Multi-Select",
+  type: "multiSelect",
+  uri: "repo:/public/Samples/Resources/Input_Controls/ProductFamily",
+  label: "ProductFamily",
+  mandatory: true,
+  readOnly: false,
+  visible: true,
+  masterDependencies: [],
+  slaveDependencies: [],
+  validationRules: [
+    {
+      mandatoryValidationRule: {
+        errorMessage: "This field is mandatory so you must enter data.",
+      },
+    },
+  ],
+  state: {
+    uri: "/public/Samples/Resources/Input_Controls/ProductFamily",
+    id: "ProductFamily",
+    options: [
+      {
+        selected: true,
+        label: "Drink",
+        value: "Drink",
+      },
+      {
+        selected: true,
+        label: "Food",
+        value: "Food",
+      },
+      {
+        selected: true,
+        label: "Non-Consumable",
+        value: "Non-Consumable",
+      },
+    ],
+  },
+};
+
+describe("MultiSelectInputControl tests", () => {
+  beforeEach(() => {
+    cleanup();
+  });
+  it("should render the MultiSelectInputControl component", () => {
+    render(<MultiSelectInputControl {...requiredProps} />);
+    const labelElement = screen.getByText("ProductFamily");
+    expect(labelElement).toBeInTheDocument();
+  });
+
+  it("should display options when clicked", async () => {
+    render(<MultiSelectInputControl {...requiredProps} />);
+    const inputElement = screen.getByLabelText("ProductFamily");
+    await act(async () => {
+      userEvent.click(inputElement);
+    });
+    const options = await screen.findAllByRole("option");
+    expect(options.length).toBe(3);
+  });
+
+  it("should select an option when clicked", async () => {
+    await act(async () => {
+      render(<MultiSelectInputControl {...requiredProps} />);
+    });
+    const inputElement = screen.getByLabelText("ProductFamily");
+    await act(async () => {
+      userEvent.click(inputElement);
+    });
+    const optionElement = await screen.findByText("Drink");
+    await act(async () => {
+      await userEvent.click(optionElement);
+    });
+    const liEl = document.querySelector("ul li#Drink");
+    expect(liEl).toHaveAttribute("aria-selected", "true");
+  });
+
+  it("should display error message when there is an error", async () => {
+    await act(async () => {
+      render(<MultiSelectInputControl {...requiredProps} />);
+    });
+    const inputElement = screen.getByLabelText("ProductFamily");
+    await act(async () => {
+      userEvent.click(inputElement);
+    });
+    const optionElement = await screen.findByText("Drink");
+    await act(async () => {
+      await userEvent.click(optionElement);
+    });
+    await act(async () => {
+      await userEvent.click(optionElement);
+    });
+    const errorElement = screen.getByText(
+      "This field is mandatory so you must enter data.",
+    );
+    expect(errorElement).toBeInTheDocument();
+  });
+});
