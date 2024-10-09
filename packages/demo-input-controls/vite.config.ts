c0c547e5bcea31daed3381e1ae6f4de3daf2df09
Eduardo Canche Vazquez <ecanchev@tibco.com>
2024-10-09 14:03:54 -0600

    fix(demo-input-controls): remove libcss package for demo-input-controls
    the styles live in the jv-input-controls package

--- packages/demo-input-controls/vite.config.ts
+++ packages/demo-input-controls/vite.config.ts
@@ -5,12 +5,11 @@
  */
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import libCss from "vite-plugin-libcss";
 import { dependencies } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), libCss()],
+  plugins: [react()],
   build: {
     rollupOptions: {
       external: [...Object.keys(dependencies || {}), "react/jsx-runtime"],
